0159c8cf55dae0431c6c8b6c0df758fcbb75dea4
Vince Andrew Pimentel <contact@vincep.me>
2018-12-27 02:18:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,13 +8,13 @@ class Application
     num_3 = Kernel.rand(1..20)
 
     if num_1 == num_2 && num_2 == num_3
-      resp.write "#{num_1} - #{num_2} - #{num_3}"
+      resp.write "#{num_1} - #{num_2} - #{num_3}\n"
       resp.write "You win!"
     else
-      resp.write "#{num_1} - #{num_2} - #{num_3}"
+      resp.write "#{num_1} - #{num_2} - #{num_3}\n"
       resp.write "You lose!"
     end
-    
+
     resp.finish
   end
 
